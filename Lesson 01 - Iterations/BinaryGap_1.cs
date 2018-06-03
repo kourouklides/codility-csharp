f7cc78ac152a885a615e7198ad306e744e40ee1e
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-03 06:57:52 +0300

    Update BinaryGap_1.cs

--- Lesson 01 - Iterations/BinaryGap_1.cs
+++ Lesson 01 - Iterations/BinaryGap_1.cs
@@ -11,20 +11,21 @@ class Solution
     {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
         
-        string binary = Convert.ToString(N, 2);
+        string N_binary = Convert.ToString(N, 2);
     
-        string[] gaps = binary.Split('1');
+        string[] gaps = N_binary.Split('1');
     
-        string biggestGap = "";
-        for (int i = 0; i < (binary.EndsWith("1") ? gaps.Length : gaps.Length-1); i++)
+        string max_gap = "";
+        int repetitions = (N_binary.EndsWith("1") ? gaps.Length : gaps.Length-1);
+        for (int i = 0; i < repetitions; i++)
         {
-            if (gaps[i].Contains("0") && gaps[i].Length > biggestGap.Length )
+            if (gaps[i].Contains("0") && gaps[i].Length > max_gap.Length )
             {
-                biggestGap = gaps[i];
+                max_gap = gaps[i];
             }
         }
     
-        return biggestGap.Length;
+        return max_gap.Length;
     }
 }
 
