ed9daf9c0c6b18823a9bc53f82bfde680761ec23
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-03 05:47:05 +0300

    Rename BinaryGap_2.cs to BinaryGap_3.cs

---
+++ Lesson 01 - Iterations/BinaryGap_3.cs
@@ -0,0 +1,45 @@
+using System;
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int N)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        
+        int i = N;
+        int counter = 0;
+        int result = 0;
+        bool found_one = false;
+        
+        while (i > 0)
+        {
+            if ( (i & 1) == 1)
+            {
+                if (found_one == false)
+                {
+                    found_one = true;
+                }
+                else
+                {
+                    result = Math.Max(result, counter);
+                }
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+            
+            i >>= 1;
+        }
+        
+        return result;
+    
+    }
+}
+
