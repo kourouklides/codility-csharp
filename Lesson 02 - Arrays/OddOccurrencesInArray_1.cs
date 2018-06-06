02057ad7ef4c456c0866ac9149058b9b4d1d4983
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-06 04:26:21 +0300

    Update OddOccurrencesInArray_1.cs

--- Lesson 02 - Arrays/OddOccurrencesInArray_1.cs
+++ Lesson 02 - Arrays/OddOccurrencesInArray_1.cs
@@ -11,7 +11,7 @@ class Solution {
     public int solution(int[] A) {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
         
-        var uniqueInt = A.GroupBy(x=>x).Where(x => x.Count() == 1).Select(x => x.Key).ToList();
+        var uniqueInt = A.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).ToList();
         
         return uniqueInt[0];
     }
