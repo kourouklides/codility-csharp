dbf079852dff97edeca3f47e3a0f68074d6da0ee
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-06 04:23:49 +0300

    Create OddOccurrencesInArray_1.cs

---
+++ Lesson 02 - Arrays/OddOccurrencesInArray_1.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+
+// you can also use other imports, for example:
+// using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution {
+    public int solution(int[] A) {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        
+        var uniqueInt = A.GroupBy(x=>x).Where(x => x.Count() == 1).Select(x => x.Key).ToList();
+        
+        return uniqueInt[0];
+    }
+}
+
