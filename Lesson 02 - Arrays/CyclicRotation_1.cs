40e2d77737f66b8cd1d2c5970e9889239d5dc7cc
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-06 03:25:38 +0300

    Update CyclicRotation_1.cs

--- Lesson 02 - Arrays/CyclicRotation_1.cs
+++ Lesson 02 - Arrays/CyclicRotation_1.cs
@@ -1,14 +1,23 @@
 using System;
+using System.Linq;
 // you can also use other imports, for example:
 // using System.Collections.Generic;
 
 // you can write to stdout for debugging purposes, e.g.
 // Console.WriteLine("this is a debug message");
 
-class Solution
-{
-    public int[] solution(int[] A, int K)
-    {
+class Solution {
+    public int[] solution(int[] A, int K) {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
         
+        int n = A.Length;
+        int[] B = new int[n];
+        
+        for (int i=0; i<n; i++)
+        {
+            B[(i + K) % n] = A[i];
+        }
+        
+        return B;
     }
 }
