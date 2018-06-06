6c7b67b374c2d93f7d3e39265c59fbc779332446
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-06 03:06:51 +0300

    Update CyclicRotation_2.cs

--- Lesson 02 - Arrays/CyclicRotation_2.cs
+++ Lesson 02 - Arrays/CyclicRotation_2.cs
@@ -1,14 +1,38 @@
 using System;
+using System.Linq;
 // you can also use other imports, for example:
 // using System.Collections.Generic;
 
 // you can write to stdout for debugging purposes, e.g.
 // Console.WriteLine("this is a debug message");
 
-class Solution
-{
-    public int[] solution(int[] A, int K)
-    {
+class Solution {
+    public int[] solution(int[] A, int K) {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
         
+        int N = A.Length;
+        
+        if ((N==0) || (K==0) || (K==N))
+        {
+            return A;
+        }
+        
+        int i;
+        
+        if (K > N)
+        {
+            i = K % N;
+        }
+        else
+        {
+            i = K;
+        }
+        
+        int[] A1 = A.Skip(N-i).Take(i).ToArray();
+        int[] A2 = A.Skip(0).Take(N-i).ToArray();
+        
+        int[] B = A1.Concat(A2).ToArray();
+        
+        return B;
     }
 }
