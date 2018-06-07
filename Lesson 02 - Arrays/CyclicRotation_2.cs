576d547f8999aefa42a81faa9516b2bf5e34fb5b
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-07 06:29:06 +0300

    Update CyclicRotation_2.cs

--- Lesson 02 - Arrays/CyclicRotation_2.cs
+++ Lesson 02 - Arrays/CyclicRotation_2.cs
@@ -7,8 +7,10 @@ using System.Linq;
 // you can write to stdout for debugging purposes, e.g.
 // Console.WriteLine("this is a debug message");
 
-class Solution {
-    public int[] solution(int[] A, int K) {
+class Solution
+{
+    public int[] solution(int[] A, int K)
+    {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
         
         int N = A.Length;
