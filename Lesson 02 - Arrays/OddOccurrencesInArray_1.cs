fe6b4191c7b56ee364b3265d67eb5b74f46f1bec
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-07 06:28:46 +0300

    Update OddOccurrencesInArray_1.cs

--- Lesson 02 - Arrays/OddOccurrencesInArray_1.cs
+++ Lesson 02 - Arrays/OddOccurrencesInArray_1.cs
@@ -7,8 +7,10 @@ using System.Linq;
 // you can write to stdout for debugging purposes, e.g.
 // Console.WriteLine("this is a debug message");
 
-class Solution {
-    public int solution(int[] A) {
+class Solution
+{
+    public int solution(int[] A)
+    {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
         
         var uniqueInt = A.GroupBy(x => x).Where(x => x.Count() == 1).Select(x => x.Key).ToList();
