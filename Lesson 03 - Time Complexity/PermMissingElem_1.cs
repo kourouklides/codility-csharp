c6744dcbbd044aef199a8dc1ced1c693b0f49bec
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-07 07:43:40 +0300

    Update PermMissingElem_1.cs

--- Lesson 03 - Time Complexity/PermMissingElem_1.cs
+++ Lesson 03 - Time Complexity/PermMissingElem_1.cs
@@ -13,16 +13,16 @@ class Solution
         // write your code in C# 6.0 with .NET 4.5 (Mono)
         
         
-        int N = A.Length;
+        int N = A.Length + 1;
         
         int sum1 = 0;
         int sum2 = 0;
-        for (int i=0; i < N; i++)
+        for (int i=1; i < N; i++)
         {
-            sum1 += A[i];
-            sum2 += i+1;
+            sum1 += A[i-1];
+            sum2 += i;
         }
-        sum2 += N+1;
+        sum2 += N;
         
         int missing = sum2 - sum1;
         
