dcea524cf3bd2f2196433c51062191f30ca1e30b
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-07 06:28:29 +0300

    Update OddOccurrencesInArray_2.cs

--- Lesson 02 - Arrays/OddOccurrencesInArray_2.cs
+++ Lesson 02 - Arrays/OddOccurrencesInArray_2.cs
@@ -6,8 +6,10 @@ using System.Linq;
 // you can write to stdout for debugging purposes, e.g.
 // Console.WriteLine("this is a debug message");
 
-class Solution {
-    public int solution(int[] A) {
+class Solution
+{
+    public int solution(int[] A)
+    {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
         
         int N = A.Length;
