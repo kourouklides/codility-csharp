d084862ead57f894364f7cca938761fa07939a9a
Ioannis Kourouklides <kourouklides@users.noreply.github.com>
2018-06-11 04:49:44 +0300

    Update TapeEquilibrium_1.cs

--- Lesson 03 - Time Complexity/TapeEquilibrium_1.cs
+++ Lesson 03 - Time Complexity/TapeEquilibrium_1.cs
@@ -11,8 +11,6 @@ class Solution
     {
         // write your code in C# 6.0 with .NET 4.5 (Mono)
         
-        int N = A.Length;
-        
         int index1 = 0;
         int index2 = N-1;
         
